79ba9de3392f2d67435d4c40998d429d2512bfe4
Chloe Everson <chloeliz93@gmail.com>
2021-05-07 15:50:25 -0500

    created components and routes for each feedback view

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,14 @@
+//importing libraries
 import React from 'react';
 import axios from 'axios';
+
+
+
+//importing components 
+import Feeling from '../Feeling/Feeling';
+import Understanding from '../Understanding/Understanding';
+import Support from '../Support/Support';
+import Comments from '../Comments/Comments';
 import './App.css';
 
 function App() {
@@ -10,6 +19,21 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      {/* making routes / sourcing components */}
+      <Router>
+        <Route path="/" exact>
+          <Feeling />
+        </Route>
+        <Route path="/understanding" exact>
+          <Understanding />
+        </Route>
+        <Route path="/support" exact>
+          <Support />
+        </Route>
+        <Route path="/comments" exact>
+          <Comments />
+        </Route>
+      </Router>
     </div>
   );
 }
